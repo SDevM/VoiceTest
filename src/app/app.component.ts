4c126207cc1a584fa342363776c39aa615028057
SDevM <lolicongress@outlook.com>
2023-03-10 07:34:51 -0500

    ~ pause and resume button rigged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,14 @@ export class AppComponent {
     }
     console.log('VOICE ACTIVE', this.voiceActive);
   }
+
+  toggle() {
+    if (this.paused) {
+      this.paused = false;
+      this.audioRecorder.resume();
+    } else if (!this.paused) {
+      this.paused = true;
+      this.audioRecorder.pause();
+    }
+  }
 }
