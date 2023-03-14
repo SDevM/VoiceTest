383b3952e4da7b240ff2d3c00ad4f33703d0b298
SDevM <lolicongress@outlook.com>
2023-03-14 16:17:41 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,13 +39,13 @@ export class AppComponent {
         } else {
           console.log('Offer', session);
 
-          pc.setRemoteDescription(session).catch((err) =>
-            console.error(err.message)
-          );
           pc.createAnswer().then((answer) => {
             // pc!.setLocalDescription(answer);
             sService.sendSession(answer, id, true);
           });
+          pc.setRemoteDescription(session).catch((err) =>
+            console.error(err.message)
+          );
         }
       }
     );
